cfca460aff73083f195da8a3572d344900f4a451
MEI YUK YUNG <m.y.yung@tilburguniversity.edu>
2020-06-01 15:14:31 +0200

    preclean and analysis is updated

--- src/analysis/preclean.R
+++ src/analysis/preclean.R
@@ -14,6 +14,28 @@ dt[, datetime:=paste(substr(timestamp, 5, 10),substr(timestamp, 27, 30),substr(t
 #dt[, time:=]
 
 dt[, datetime:=AsDateTime(datetime)]
+dt[, datetime:=gsub("T", " ", datetime)] 
+dt[, datetime:=gsub("Z", "", datetime)]
+dt[, event:= if_else(strftime(datetime, format = "%Y-%m-%d %H:%M:%S") <= 
+    strftime("2020-04-23 23:25:00",format = "%Y-%m-%d %H:%M:%S"), "1", "")]
+dt[,event:=if_else(strftime(datetime, format = "%Y-%m-%d %H:%M:%S") <= 
+    strftime("2020-04-24 14:25:00",format = "%Y-%m-%d %H:%M:%S") & 
+    strftime(datetime, format = "%Y-%m-%d %H:%M:%S") >= 
+    strftime("2020-04-24 13:20:00",format = "%Y-%m-%d %H:%M:%S"), "2", event)] 
+dt[,event:=if_else(strftime(datetime, format = "%Y-%m-%d %H:%M:%S") <= 
+    strftime("2020-04-25 04:25:00",format = "%Y-%m-%d %H:%M:%S") & 
+    strftime(datetime, format = "%Y-%m-%d %H:%M:%S") >= 
+    strftime("2020-04-25 03:20:00",format = "%Y-%m-%d %H:%M:%S"), "3", event)]
+dt[,event:=if_else(strftime(datetime, format = "%Y-%m-%d %H:%M:%S") <= 
+    strftime("2020-04-25 15:25:00",format = "%Y-%m-%d %H:%M:%S") & 
+    strftime(datetime, format = "%Y-%m-%d %H:%M:%S") >= 
+    strftime("2020-04-25 14:20:00",format = "%Y-%m-%d %H:%M:%S"), "4", event)]
+dt[,event:=if_else(strftime(datetime, format = "%Y-%m-%d %H:%M:%S") <= 
+    strftime("2020-04-25 22:30:00",format = "%Y-%m-%d %H:%M:%S") & 
+    strftime(datetime, format = "%Y-%m-%d %H:%M:%S") >= 
+    strftime("2020-04-25 21:20:00",format = "%Y-%m-%d %H:%M:%S"), "5", event)]
+dt[,event:=as.factor(event)]
+
 
 
 dir.create('../../gen/analysis/temp/', recursive = TRUE)
